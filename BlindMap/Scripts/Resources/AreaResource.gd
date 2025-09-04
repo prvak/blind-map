55537afe200ba05b83e26bf5cb9cdfd74f048349
Michal Mašek <misamasek@gmail.com>
2025-09-04 09:53:45 +0200

    Use typed dictionary for language priorities

--- BlindMap/Scripts/Resources/AreaResource.gd
+++ BlindMap/Scripts/Resources/AreaResource.gd
@@ -3,6 +3,6 @@ class_name AreaResource extends Resource
 
 @export var Id: String
 @export var Name: String
-@export var LanguagePriorities: Dictionary
+@export var LanguagePriorities: Dictionary[StringName, int]
 @export var MiniMapTexture: Texture2D
 @export var LevelSequences: Array[LevelSequenceResource] = []
